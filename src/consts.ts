5e4efecf60bcdf254c31a5d5e2d01092ee66592a
Ivan Koryshkin <ivan.koryshkin@gmail.com>
2023-11-03 10:51:41 +0400

    move api url to env

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8000/api/v1"
+export const API_URL = process.env.BACKEND_URL ? process.env.BACKEND_URL : "http://localhost:5000/api/v1";
 export const API_USER_LIST = `${API_URL}/customer/sync`
 export const API_ACCOUNT_LIST = `${API_URL}/public/account`
