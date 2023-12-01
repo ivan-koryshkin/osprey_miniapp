696c59aef7e6c62f8952bd4f79c7cfb1eac43c4d
Ivan Koryshkin <ivan.koryshkin@gmail.com>
2023-12-02 00:35:51 +0400

    gh pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,3 @@
-// export const API_URL = 'https://osprey-hub.ru/api/v1'
-export const API_URL = 'http://localhost:5000/api/v1'
+export const API_URL = 'https://osprey-hub.ru/api/v1'
 export const API_USER_LIST = `${API_URL}/customer/sync`
 export const API_ACCOUNT_LIST = `${API_URL}/public/account`
