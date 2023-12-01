3586a16b9cb50cc88919abc1d173412bd1cb59da
Ivan Koryshkin <ivan.koryshkin@gmail.com>
2023-12-02 00:45:03 +0400

    Updates

--- src/app.hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import type { AppState, AppDispatch } from './store/store'
-
-type DispatchFunc = () => AppDispatch
-export const useAppDispatch: DispatchFunc = useDispatch
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
--- src/consts.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://osprey-hub.ru/api/v1'
-export const API_USER_LIST = `${API_URL}/customer/sync`
-export const API_ACCOUNT_LIST = `${API_URL}/public/account`
